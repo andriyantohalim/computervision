17c41a185c941c950213a7f9d2084fd74442eb15
Andriyanto Halim <andriyanto.halim@gmail.com>
2018-02-28 11:26:28 +0800

    Update install-caffe_mod.sh

--- install-caffe_mod.sh
+++ install-caffe_mod.sh
@@ -63,6 +63,7 @@ if [ -z $WORK ]; then WORK=$PWD;fi
 
 
 INFO "Step 1a. Install dependencies"
+RUN sudo apt-get install fort77 gfortran
 RUN sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
 RUN sudo apt-get install --no-install-recommends libboost-all-dev
 RUN sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
